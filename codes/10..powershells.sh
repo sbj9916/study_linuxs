11445a16a4e02e88b364e3070eedd728ce0df265
sbj9916 <sbj9916@gmail.com>
2025-07-15 12:23:07 +0900

    두번쨰 올리기

---
+++ codes/10..powershells.sh
@@ -0,0 +1,19 @@
+PS C:\Users\Administrator> cd C:\
+PS C:\> cd C:\Develops
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15  오전 11:38                Downloads
+d-----      2025-07-15  오전 11:50                VMs
