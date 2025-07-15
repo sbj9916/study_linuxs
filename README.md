c53e52cddc5df5a0a7ccf0616a046a7782977ef6
sbj9916 <sbj9916@gmail.com>
2025-07-15 14:33:14 +0900

    file & directory command

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 - 첫번째
 - 두번째
 
-[안내글](codes/10..powershells.sh)
+- Searching command : [안내글](codes/10..powershells.sh)
+- file & directory command : [명령어](codes/20..controll_file_dir_powershells.sh)
+
+## Quests
--- codes/20..controll_file_dir_powershells.sh
+++ codes/20..controll_file_dir_powershells.sh
@@ -1,22 +1,12 @@
-PS C:\Users\Administrator> cd C:\
-PS C:\> cd C:\Develops
-PS C:\Develops> pwd
-
-Path
-----
-C:\Develops
+PS C:\Develops\temp_dir> mkdir dir_first
 
 
-PS C:\Develops> ls
-
-
-    디렉터리: C:\Develops
+    디렉터리: C:\Develops\temp_dir
 
 
 Mode                 LastWriteTime         Length Name
 ----                 -------------         ------ ----
-d-----      2025-07-15  오전 11:38                Downloads
-d-----      2025-07-15  오전 11:50                VMs
+d-----      2025-07-15   오후 2:31                dir_first
 
 PS C:\Develops\temp_dir> ls
 
